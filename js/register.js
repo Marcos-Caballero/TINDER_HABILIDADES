f8890eff4a1ae7ba9e82dbb2b4d138af87a5dc93
Marcos-Caballero <robayo1100@gmail.com>
2023-09-13 16:26:54 -0500

    Cambiando todo a modales, es mejor jaja

--- js/register.js
+++ js/register.js
@@ -43,3 +43,15 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     });
 });
+
+// Función para abrir un modal específico
+function openModal(type) {
+    const modal = document.getElementById(type + 'Modal');
+    modal.style.display = 'block';
+}
+
+// Función para cerrar un modal específico
+function closeModal(type) {
+    const modal = document.getElementById(type + 'Modal');
+    modal.style.display = 'none';
+}
