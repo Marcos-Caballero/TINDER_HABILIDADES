156d364f521f851bc10f2613144d5754e3efda56
Marcos-Caballero <robayo1100@gmail.com>
2023-09-14 12:16:28 -0500

    Corrigiendo cosas de enlace entre front y back

--- js/login.js
+++ js/login.js
@@ -1,30 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const loginForm = document.getElementById('loginForm');
+/* Función del modal */
+function openModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.style.display = "block";
+}
 
-    loginForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    const correo = document.getElementById('correo').value;
-    const contrasena = document.getElementById('contrasena').value;
-
-    try {
-        const response = await fetch('/api/auth/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ correo, contrasena }),
-        });
-
-        if (response.ok) {
-        const data = await response.json();
-          alert(data.mensaje); // Muestra el mensaje de éxito del backend
-        } else {
-        const data = await response.json();
-          alert(data.error); // Muestra el mensaje de error del backend
-        }
-    } catch (error) {
-        console.error('Error de red:', error);
-    }
-    });
-});
+function closeModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.style.display = "none";
+}
